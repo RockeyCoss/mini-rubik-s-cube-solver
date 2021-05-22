f9b8775f7acb2c59f1882b90453ce1f1c73b48a8
Rockey <1350291943@qq.com>
2021-05-22 18:06:31 +0800

    updated data structure

--- GroupSolver.py
+++ GroupSolver.py
@@ -1,9 +1,10 @@
 import numpy as np
-from utility import decodeCube, encodeCube, moveTable, move
+from utility import decodeCube, encodeCube, moveTable, move, MoveItem, CubeState
+
 # use IDA*
 # f(n)=g(n)+h(n)
 manhattanDistance = np.array([
-   # 0  1  2  3  4  5  6
+    # 0  1  2  3  4  5  6
     [0, 1, 2, 2, 1, 2, 3],
     [1, 0, 1, 3, 2, 1, 2],
     [2, 1, 0, 2, 3, 2, 1],
@@ -12,47 +13,74 @@ manhattanDistance = np.array([
     [2, 1, 2, 2, 1, 0, 1],
     [3, 2, 1, 1, 2, 1, 0],
 ])
-solvedState=[np.arange(7),np.zeros(7,dtype=int)]
-[2,3,1,0,4,5,6]
-def heuristicH(currentCube:[np.ndarray,np.ndarray])->float:
-    # h(n)=max(blocks' 3D manhattan distance, orientation recovery distance)
-    permutation , orientation=currentCube
-    blockDis=np.sum(manhattanDistance[permutation,np.arange(6)])/4
-    orienDis=np.sum(orientation)/4
-    return np.max(blockDis,orienDis)
+solvedState = CubeState([np.arange(7), np.zeros(7, dtype=int)])
 
-def isSolved(currentCube:[np.ndarray, np.ndarray])->bool:
-    global solvedState
-    return currentCube[0]==solvedState[0] and currentCube[1]==solvedState[1]
+
 # Phase one utility functions
-def isPhaseOneAchieved(currentCube:[np.ndarray, np.ndarray])->bool:
-    global solvedState
-    return currentCube[1]==solvedState[1]
+def isPhaseOneAchieved(currentCube: CubeState) -> bool:
+    return (currentCube.orienSequence==np.zeros(7)).all()
 
-def phaseOneH(currentCube:[np.ndarray,np.ndarray])->float:
-    _,orientation=currentCube
-    orienDis=np.sum(orientation)/4
+
+def phaseOneH(currentCube: [np.ndarray, np.ndarray]) -> float:
+    _, orientation = currentCube
+    orienDis = np.sum(orientation) / 4
     return orienDis
 
-def solve(cube:[np.ndarray, np.ndarray])->list:
-    if isSolved(cube):
+
+# Phase two utility functions
+def heuristicH(currentCube: [np.ndarray, np.ndarray]) -> float:
+    # h(n)=max(blocks' 3D manhattan distance, orientation recovery distance)
+    permutation, orientation = currentCube
+    blockDis = np.sum(manhattanDistance[permutation, np.arange(6)]) / 4
+    orienDis = np.sum(orientation) / 4
+    return np.max(blockDis, orienDis)
+
+
+def isCompletelySolved(currentCube: CubeState) -> bool:
+    global solvedState
+    return currentCube==solvedState
+
+
+def solve(cube: [np.ndarray, np.ndarray]) -> list:
+    if isCompletelySolved(cube):
         return []
 
-    ancesChoice=None
+    ancesChoice = None
     # record the times of a movement being chose
-    choseTime=0
+    choseTime = 0
     # phase one
     # from <R,U,F> to <U,R2,F2>
-    for maxDepth in range(1,20):
-        movementLog=[]
-        for movement in moveTable:
-            length=len(movement)
-            if length==0 or length!=0 and movementLog[-1]!=movement:
-                choseTime=1
-            else:
-                choseTime+=1
+    for maxDepth in range(1, 20):
+        movementLog = []
+        # for movement in moveTable:
+        #     length = len(movement)
+        #     if length == 0 or length != 0 and movementLog[-1] != movement:
+        #         choseTime = 1
+        #     else:
+        #         choseTime += 1
 
 
-def dps(maxLength:int,cube:[np.ndarray, np.ndarray]):
-    pass
+def phaseOneDps(maxDepth: int,currentDepth:int,cube: [np.ndarray, np.ndarray],movementLog:list,choseTime:int,preChoice:MoveItem):
+    global moveTable
+    # exceed max depth, prune
+    if currentDepth>maxDepth:
+        return
+    for movement in moveTable:
+        # R R', prune
+        if movement==preChoice:
+            continue
+        length=len(movement)
+        if length==0 or length!=0 and movementLog[-1]!=movement:
+            choseTime=1
+        else:
+            choseTime+=1
+        # R R R = R', prune
+        if choseTime==3:
+            return
+        newCube=
+
+
+
+
+
 
--- utility.py
+++ utility.py
@@ -1,7 +1,37 @@
 from collections import namedtuple
-
+from typing import List
 import vpython as vp
 import numpy as np
+
+MoveItemBase = namedtuple("MoveItem", ["permutation", "orientation"])
+CubeStateBase = namedtuple("CubeState",["blockSequence","orienSequence"])
+class MoveItem(MoveItemBase):
+    def __new__(cls, permutation:np.ndarray,orientation:np.ndarray,copy=True):
+        if copy:
+            return super(MoveItem, cls).__new__(cls,permutation=permutation.copy(),orientation=orientation.copy())
+        else:
+            return super(MoveItem, cls).__new__(cls, permutation=permutation, orientation=orientation)
+    def __eq__(self, other):
+        return (self[0]==other[0]).all() and (self[1]==other[1]).all()
+
+class CubeState(CubeStateBase):
+
+    def __new__(cls, *args, blockSequence:np.ndarray=None,orienSequence:np.ndarray=None,copy=True,**kwargs):
+        if len(args)==1 and (type(args[0])==list or type(args[0])==tuple):
+            if copy:
+                return super(CubeState,cls).__new__(cls,blockSequence=args[0][0].copy(),orienSequence=args[0][1].copy())
+            else:
+                return super(CubeState, cls).__new__(cls, blockSequence=args[0][0],
+                                                     orienSequence=args[0][1])
+        else:
+            if copy:
+                return super(CubeState,cls).__new__(cls,blockSequence=blockSequence.copy(),orienSequence=orienSequence.copy())
+            else:
+                return super(CubeState, cls).__new__(cls, blockSequence=blockSequence,
+                                                     orienSequence=orienSequence)
+    def __eq__(self, other):
+        return (self[0]==other[0]).all() and (self[1]==other[1]).all()
+
 def rankPermutation(p: np.ndarray):
     p=p.copy()
     pM1 = p.argsort()
@@ -25,12 +55,13 @@ def unrankPermutation(r: int, n: int) -> np.ndarray:
     return p
 
 
-def encodeCube(pie: np.ndarray, q: np.ndarray) -> int:
+def encodeCube(cube:CubeState) -> int:
+    pie,q=cube
     perNum = rankPermutation(pie)
     return int(perNum * np.power(3, 6) + np.sum(q[:6] * np.array([np.power(3, i) for i in range(5, -1, -1)])))
 
 
-def decodeCube(cubeNum: int) -> (np.ndarray, np.ndarray):
+def decodeCube(cubeNum: int) -> CubeState:
     perNum, remain = divmod(cubeNum, np.power(3, 6))
     permutation = unrankPermutation(perNum, 7)
     orientation = []
@@ -43,10 +74,11 @@ def decodeCube(cubeNum: int) -> (np.ndarray, np.ndarray):
     # sum(orientation)=k
     # (3-k%3)%3=(3%3-k%3)%3=(-k%3)%3=(-1%3*k%3)%3=(2*k%3)%3
     orientation.append((2*(sum(orientation) % 3)) % 3)
-    return permutation,np.array(orientation,dtype=int)
+    return CubeState([permutation,np.array(orientation,dtype=int)],copy=False)
 
 
-MoveItem = namedtuple("MoveItem", ["permutation", "orientation"])
+
+
 moveTable = {
     # U
     'U': MoveItem(permutation=np.array([0, 1, 2, 6, 3, 4, 5]), orientation=np.array([0, 0, 0, 0, 0, 0, 0])),
@@ -62,7 +94,7 @@ moveTable = {
     "F'": MoveItem(permutation=np.array([0, 2, 6, 3, 4, 1, 5]), orientation=np.array([0, 1, 2, 0, 0, 2, 1])),
 }
 
-def move(cube:[np.ndarray,np.ndarray],movement:MoveItem)->[np.ndarray,np.ndarray]:
-    newPermutation=cube[0][movement.permutation]
-    newOrientation=np.mod((cube[1][movement.permutation]+movement.orientation),3)
-    return [newPermutation,newOrientation]
+def move(cube:CubeState,movement:MoveItem)->[np.ndarray,np.ndarray]:
+    newPermutation=cube.blockSequence[movement.permutation]
+    newOrientation=np.mod((cube.orienSequence[movement.permutation]+movement.orientation),3)
+    return CubeState([newPermutation,newOrientation],copy=False)
