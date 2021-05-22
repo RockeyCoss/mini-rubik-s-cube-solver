bbc6e1684a0c36b5af60617df0f9c8ab7a6d0253
Rockey <1350291943@qq.com>
2021-05-22 16:37:37 +0800

    writing IDA*

--- GroupSolver.py
+++ GroupSolver.py
@@ -24,12 +24,34 @@ def heuristicH(currentCube:[np.ndarray,np.ndarray])->float:
 def isSolved(currentCube:[np.ndarray, np.ndarray])->bool:
     global solvedState
     return currentCube[0]==solvedState[0] and currentCube[1]==solvedState[1]
+# Phase one utility functions
+def isPhaseOneAchieved(currentCube:[np.ndarray, np.ndarray])->bool:
+    global solvedState
+    return currentCube[1]==solvedState[1]
+
+def phaseOneH(currentCube:[np.ndarray,np.ndarray])->float:
+    _,orientation=currentCube
+    orienDis=np.sum(orientation)/4
+    return orienDis
+
 def solve(cube:[np.ndarray, np.ndarray])->list:
     if isSolved(cube):
         return []
-    maxDepth=1
+
     ancesChoice=None
-    preChoice=None
+    # record the times of a movement being chose
+    choseTime=0
+    # phase one
+    # from <R,U,F> to <U,R2,F2>
+    for maxDepth in range(1,20):
+        movementLog=[]
+        for movement in moveTable:
+            length=len(movement)
+            if length==0 or length!=0 and movementLog[-1]!=movement:
+                choseTime=1
+            else:
+                choseTime+=1
+
 
 def dps(maxLength:int,cube:[np.ndarray, np.ndarray]):
     pass
